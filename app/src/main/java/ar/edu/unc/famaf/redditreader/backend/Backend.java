8c10be8c19e52129eb831f6b3bf7c796847b2833
Emiliano <emikokic@gmail.com>
2016-10-09 23:04:30 -0300

    Implementacion de ViewHolder

--- app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/backend/Backend.java
@@ -22,35 +22,35 @@ public class Backend {
         PostModel postModel1 = new PostModel();
         postModel1.setTitle("Este es el titulo numero 1");
         postModel1.setAuthor("r/author1");
-        postModel1.setDate("Hace 1hora");
+        postModel1.setDate("Hace 1 hora");
         postModel1.setComments(10);
         postModel1.setPreviewId(R.drawable.reddit_icon);
 
         PostModel postModel2 = new PostModel();
         postModel2.setTitle("Este es el titulo numero 2");
         postModel2.setAuthor("r/author2");
-        postModel2.setDate("Hace 2horas");
+        postModel2.setDate("Hace 2 horas");
         postModel2.setComments(20);
         postModel2.setPreviewId(R.drawable.reddit_icon);
 
         PostModel postModel3 = new PostModel();
         postModel3.setTitle("Este es el titulo numero 3");
         postModel3.setAuthor("r/author3");
-        postModel3.setDate("Hace 3horas");
+        postModel3.setDate("Hace 3 horas");
         postModel3.setComments(30);
         postModel3.setPreviewId(R.drawable.reddit_icon);
 
         PostModel postModel4 = new PostModel();
         postModel4.setTitle("Este es el titulo numero 4");
         postModel4.setAuthor("r/author4");
-        postModel4.setDate("Hace 4horas");
+        postModel4.setDate("Hace 4 horas");
         postModel4.setComments(40);
         postModel4.setPreviewId(R.drawable.reddit_icon);
 
         PostModel postModel5 = new PostModel();
         postModel5.setTitle("Este es el titulo numero 5");
         postModel5.setAuthor("r/author5");
-        postModel5.setDate("Hace 5horas");
+        postModel5.setDate("Hace 5 horas");
         postModel5.setComments(50);
         postModel5.setPreviewId(R.drawable.reddit_icon);
 
--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/PostAdapter.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/PostAdapter.java
@@ -42,28 +42,53 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        ViewHolder viewHolder = null;
+
         if (convertView == null) {
             LayoutInflater vi = (LayoutInflater) getContext().
                     getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
             convertView = vi.inflate(R.layout.post_row, null);
+
+            TextView titleTV = (TextView) convertView.findViewById(R.id.titleTV);
+            TextView authorTV = (TextView) convertView.findViewById(R.id.authorTV);
+            TextView commentsTV = (TextView) convertView.findViewById(R.id.commentsTV);
+            TextView dateTV = (TextView) convertView.findViewById(R.id.dateTV);
+            ImageView previewIV = (ImageView) convertView.findViewById(R.id.previewIV);
+            viewHolder = new ViewHolder(titleTV, authorTV, commentsTV, dateTV, previewIV);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
         PostModel pm = postList.get(position);
 
-        TextView titleText = (TextView) convertView.findViewById(R.id.titleTV);
-        TextView authorText = (TextView) convertView.findViewById(R.id.authorTV);
-        TextView commentsText = (TextView) convertView.findViewById(R.id.commentsTV);
-        TextView dateText = (TextView) convertView.findViewById(R.id.dateTV);
-        ImageView previewImageId = (ImageView) convertView.findViewById(R.id.previewIV);
-
-        titleText.setText(pm.getTitle());
-        authorText.setText(pm.getAuthor());
-        commentsText.setText(String.valueOf(pm.getComments()));
-        dateText.setText(pm.getDate());
-        previewImageId.setImageResource(pm.getPreviewId());
+        viewHolder.titleTV.setText(pm.getTitle());
+        viewHolder.authorTV.setText(pm.getAuthor());
+        viewHolder.commentsTV.setText(String.valueOf(pm.getComments()));
+        viewHolder.dateTV.setText(pm.getDate());
+        viewHolder.previewIV.setImageResource(pm.getPreviewId());
 
         return convertView;
+    }
+
+    private class ViewHolder {
+        public final TextView titleTV;
+        public final TextView authorTV;
+        public final TextView commentsTV;
+        public final TextView dateTV;
+        public final ImageView previewIV;
+
+        public ViewHolder(TextView titleTV, TextView authorTV, TextView commentsTV,
+                          TextView dateTV, ImageView previewIV) {
+
+            this.titleTV = titleTV;
+            this.authorTV = authorTV;
+            this.commentsTV = commentsTV;
+            this.dateTV = dateTV;
+            this.previewIV = previewIV;
+        }
+
     }
 
 
